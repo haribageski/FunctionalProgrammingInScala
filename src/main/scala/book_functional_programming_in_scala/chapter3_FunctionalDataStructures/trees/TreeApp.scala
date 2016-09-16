7e972e87042a726df3a24794eab45c57e1749046
Haralampi Bageski <hari.bageski@ing.nl>
2016-09-16 09:36:51 +0200

    wip

---
+++ src/main/scala/book_functional_programming_in_scala/chapter3_FunctionalDataStructures/trees/TreeApp.scala
@@ -0,0 +1,11 @@
+//package book_functional_programming_in_scala.chapter3_FunctionalDataStructures.trees
+//
+//object TreeApp extends App{
+//  val tree = Branch(3, Branch(2, Leaf(1), Leaf(2)), Leaf(3))
+//
+//  println("size:" + tree.size())
+//  println("sizeWithFold:" + tree.sizeWithFold())
+//  println("widthWithFold:" + tree.widthWithFold())
+//  println("maxWithFold:" + tree.maxWithFold())
+//
+//}
