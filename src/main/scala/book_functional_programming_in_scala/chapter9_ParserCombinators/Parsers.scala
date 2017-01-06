c470be759d70775c27a29ab9829e8ad7f86d0324
Haralampi Bageski <hari.bageski@lunatech.com>
2017-01-06 01:41:43 +0100

    Ex. 9.6.

--- src/main/scala/book_functional_programming_in_scala/chapter9_ParserCombinators/Parsers.scala
+++ src/main/scala/book_functional_programming_in_scala/chapter9_ParserCombinators/Parsers.scala
@@ -3,6 +3,8 @@ package book_functional_programming_in_scala.chapter9_ParserCombinators
 import book_functional_programming_in_scala.chapter8_PropertyBasedTesting.{Gen, Prop}
 import book_functional_programming_in_scala.chapter8_PropertyBasedTesting.Prop._
 
+import scala.util.matching.Regex
+
 trait Parsers[ParserError, Parser[+ _]]{ self =>    //[+ _] is used when the outer type is a type constructor itself
 
   def run[A](p: Parser[A])(input: String): Either[ParserError, A]   //representation
@@ -11,6 +13,10 @@ trait Parsers[ParserError, Parser[+ _]]{ self =>    //[+ _] is used when the out
   def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)]   //primitive, lazy second argument is necessary, otherwise map2() will never terminate
   def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = product(p, p2).map(f.tupled) //lazy second argument is necessary, otherwise many() will never terminate
   def succeed[A](unit: A): Parser[A] = string("").map(_ => unit)
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]   //primitive
+  implicit def regex(r: Regex): Parser[String]
+  def thatManyChars(c: Char) = "[0-9]*".r
+    .flatMap(strMatched => listOfN(strMatched.toInt, char(c)))
 
   implicit def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
   implicit def string(s: String): Parser[String]    //primitive
@@ -47,7 +53,8 @@ trait Parsers[ParserError, Parser[+ _]]{ self =>    //[+ _] is used when the out
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
-    def map2[B, C](p2: Parser[B])(f: ((A, B)) => C): Parser[C] = self.map2(p, p2)(f)
+    def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
     def many: Parser[List[A]] = self.many(p)
     def many1: Parser[List[A]] = self.many1(p)
     def slice[A]: Parser[String] = self.slice(p)
